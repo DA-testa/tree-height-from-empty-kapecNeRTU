e6eb44f2f21afd2b2db72adac72dce8f39f19f6d
kapecNeRTU <janis.snikers@edu.rtu.lv>
2023-03-06 02:28:46 +0000

    izlaboti daži sīkumi

--- tree_height.py
+++ tree_height.py
@@ -1,33 +1,31 @@
 # python3
-
-import sys
-import threading
-import numpy
-import fileinput
-
 class TreeDepth(object):
     def __init__(self, parents):
         self._parents = parents #all nodes pointing to their parents
         self._node_count = len(parents) #length of list
-        self._max_height = 0 
+        self._max_height = 0
         self._heights = [None] * self._node_count
 
     def max_height(self):
-        for id, parent in enumerate(self._parents): #iet cauri visiem elementiem idx, nolasot parenta atrašanās vietu
+        #iet cauri visiem elementu id, nolasot parenta atrašanās vietu un pierakstot zaru augstumus
+        for id, parent in enumerate(self._parents):
             stack = []
             # saliekam stackā visus elementus sākot no id līdz root
-            while parent != -1 and self._heights[id] is None: #kamēr elements nav root UN nav atrasts augstums 
+            while parent != -1 and self._heights[id] is None: #kamēr nav root UN neeksistē augstums 
                 stack.append(id) # gāžam iekšā stackā elementa id
                 id, parent = parent, self._parents[parent] # ņemam elementa parentu
+ 
             # piešķiram pašreizējo augstumu
             if parent == -1:
                 height = 1
             else:
-                height = self._heights[id]
+                height = self._heights[id] #šajā brīdi agstums ir neieskaitot root
+
             # iztukšojam stacku sarakstot elementu augstumu   
             while stack:
                 height += 1
                 self._heights[stack.pop()] = height
+
             # pierakstam lielāko augstumu
             if self._max_height < height:
                 self._max_height = height
@@ -42,19 +40,18 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
 
-    nodeCount = 0
     nodes = 0
 
     inputChoice = input()
     if  inputChoice.lower()[0] == "i":
-        nodeCount = int(input("input:"))
+        input("input:")
         str = input()
         nodes = list(map(int, str.split()))
 
     elif inputChoice[0] == "F":
-        file = open("test/" + input())#open(inputChoice.split("", 1)[1])
+        #faila nosaukumam tiek pievienots relatīvais path, citādāk neizpildās testi
+        file = open("test/" + input(), mode="r", encoding="utf-8")
         data = file.readlines()
-        nodeCount = int(data[0])
         nodes = list(map(int, data[1].split()))
     
     # call the function and output it's result
